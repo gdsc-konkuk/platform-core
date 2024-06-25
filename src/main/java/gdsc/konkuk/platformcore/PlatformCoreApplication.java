fc130e8ac312bfc062e7ece18246442fc050d13d
WhitePiano <startoflateness@gmail.com>
2024-06-25 17:57:57 +0900

    init: Spring Starter

---
+++ src/main/java/gdsc/konkuk/platformcore/PlatformCoreApplication.java
@@ -0,0 +1,12 @@
+package gdsc.konkuk.platformcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PlatformCoreApplication.class, args);
+  }
+}
---
+++ src/test/java/gdsc/konkuk/platformcore/PlatformCoreApplicationTests.java
@@ -0,0 +1,11 @@
+package gdsc.konkuk.platformcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformCoreApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
