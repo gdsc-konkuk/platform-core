3ea856825b5e19cf41f4fb51c7ead9d4dcb8e66d
ekgns33 <ekgns00421@gmail.com>
2024-06-30 20:24:35 +0900

    test : CI 테스트를 위한 커밋

--- src/main/java/gdsc/konkuk/platformcore/PlatformCoreApplication.java
+++ src/main/java/gdsc/konkuk/platformcore/PlatformCoreApplication.java
@@ -8,6 +8,6 @@ public class PlatformCoreApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PlatformCoreApplication.class, args);
-    System.out.println("TESTCI");
+    System.out.println("TESTCITEST");
   }
 }
