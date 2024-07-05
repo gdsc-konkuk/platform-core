8807d16102054b206dad22d0d678f91aeec765a7
ekgns33 <ekgns00421@gmail.com>
2024-07-05 21:52:23 +0900

    feat : 생성자 파라매터 수정, Of 메소드 수정

--- src/main/java/gdsc/konkuk/platformcore/global/responses/SuccessResponse.java
+++ src/main/java/gdsc/konkuk/platformcore/global/responses/SuccessResponse.java
@@ -7,13 +7,12 @@ public class SuccessResponse extends Response {
 
 	private final Object data;
 
-	private SuccessResponse(boolean isSuccess, String message, Object data) {
-		super(isSuccess, message);
+	private SuccessResponse(String message, Object data) {
+		super(true, message);
 		this.data = data;
 	}
 
 	public static SuccessResponse of(Object data) {
-		//TODO: 성공에 대한 메세지를 하나로 통일할건지 정할 필요 있음.
-		return new SuccessResponse(true, "SUCCESS", data);
+		return new SuccessResponse("SUCCESS", data);
 	}
 }
