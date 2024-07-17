66e36963a5e1e0262fce4943cdb07e998cc87fa1
ekgns33 <ekgns00421@gmail.com>
2024-07-18 00:57:45 +0900

    feat : 회원가입 DTO추가, 멤버 컨트롤러 추가

---
+++ src/main/java/gdsc/konkuk/platformcore/application/member/exceptions/MemberRegisterRequest.java
@@ -0,0 +1,28 @@
+package gdsc.konkuk.platformcore.application.member.exceptions;
+
+import gdsc.konkuk.platformcore.domain.member.entity.Member;
+import gdsc.konkuk.platformcore.domain.member.entity.MemberRole;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class MemberRegisterRequest {
+	private String memberId;
+	private String password;
+	private String name;
+	private String email;
+	private MemberRole memberRole;
+	private int batch;
+
+	public static Member toEntity(MemberRegisterRequest request) {
+		return Member.builder()
+				.memberId(request.getMemberId())
+				.password(request.getPassword())
+				.name(request.getName())
+				.email(request.getEmail())
+				.role(request.getMemberRole())
+				.batch(request.getBatch())
+				.build();
+	}
+}
---
+++ src/main/java/gdsc/konkuk/platformcore/controller/member/MemberController.java
@@ -0,0 +1,27 @@
+package gdsc.konkuk.platformcore.controller.member;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gdsc.konkuk.platformcore.application.member.exceptions.MemberRegisterRequest;
+import gdsc.konkuk.platformcore.application.member.MemberService;
+import gdsc.konkuk.platformcore.global.responses.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@PostMapping("")
+	public ResponseEntity<SuccessResponse> signup(@RequestBody MemberRegisterRequest registerRequest) {
+		memberService.register(registerRequest);
+		return ResponseEntity.status(201).body(SuccessResponse.messageOnly());
+	}
+
+}
