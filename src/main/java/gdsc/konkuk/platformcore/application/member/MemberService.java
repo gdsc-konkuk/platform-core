f396c5d477c789e80ca86bc9aeeb452d921f877d
ekgns33 <ekgns00421@gmail.com>
2024-07-18 00:58:21 +0900

    feat : 멤버 서비스 추가, 회원가입 기능 추가

---
+++ src/main/java/gdsc/konkuk/platformcore/application/member/MemberService.java
@@ -0,0 +1,39 @@
+package gdsc.konkuk.platformcore.application.member;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import gdsc.konkuk.platformcore.application.member.exceptions.MemberRegisterRequest;
+import gdsc.konkuk.platformcore.application.member.exceptions.UserAlreadyExistException;
+import gdsc.konkuk.platformcore.domain.member.entity.Member;
+import gdsc.konkuk.platformcore.domain.member.repository.MemberRepository;
+import gdsc.konkuk.platformcore.global.exceptions.ErrorCode;
+import gdsc.konkuk.platformcore.global.responses.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	private boolean checkMemberAlreadyExist(String memberId) {
+		Optional<Member> member = memberRepository.findByMemberId(memberId);
+		return member.isPresent();
+	}
+
+	@Transactional
+	public SuccessResponse register(MemberRegisterRequest registerRequest) {
+
+		if(checkMemberAlreadyExist(registerRequest.getMemberId())) {
+			throw UserAlreadyExistException.of(ErrorCode.USER_ALREADY_EXISTS);
+		}
+
+		memberRepository.save(MemberRegisterRequest.toEntity(registerRequest));
+
+		return SuccessResponse.messageOnly();
+	}
+}
