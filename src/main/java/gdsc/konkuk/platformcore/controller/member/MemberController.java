83f4b5cae5c474d33fb8061389fee78188010b3d
ekgns33 <ekgns00421@gmail.com>
2024-07-20 21:19:48 +0900

    refactor : 매직넘버 삭제, HttpStatus사용

--- src/main/java/gdsc/konkuk/platformcore/controller/member/MemberController.java
+++ src/main/java/gdsc/konkuk/platformcore/controller/member/MemberController.java
@@ -1,12 +1,12 @@
 package gdsc.konkuk.platformcore.controller.member;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import gdsc.konkuk.platformcore.application.member.MemberRegisterRequest;
 import gdsc.konkuk.platformcore.application.member.MemberService;
 import gdsc.konkuk.platformcore.global.responses.SuccessResponse;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +18,10 @@ public class MemberController {
 
 	private final MemberService memberService;
 
-	@PostMapping("")
+	@PostMapping()
 	public ResponseEntity<SuccessResponse> signup(@RequestBody MemberRegisterRequest registerRequest) {
 		memberService.register(registerRequest);
-		return ResponseEntity.status(201).body(SuccessResponse.messageOnly());
+		return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.messageOnly());
 	}
 
 }
