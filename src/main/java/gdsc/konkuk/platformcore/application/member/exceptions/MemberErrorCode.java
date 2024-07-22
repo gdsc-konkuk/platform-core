a3a4b9a7b081dfefb374e0d26b8a43190f2522ff
ekgns33 <ekgns00421@gmail.com>
2024-07-22 13:14:26 +0900

    feat : 멤버 서비스 예외 추가와 에러코드 추가

--- src/main/java/gdsc/konkuk/platformcore/application/member/exceptions/MemberErrorCode.java
+++ src/main/java/gdsc/konkuk/platformcore/application/member/exceptions/MemberErrorCode.java
@@ -8,8 +8,9 @@ public enum MemberErrorCode implements CustomErrorCode {
 
 	USER_NOT_FOUND("사용자가 존재하지 않습니다. 다시 입력해주세요", "[ERROR] : 사용자 정보를 찾을 수 없음"),
 	INVALID_USER_INFO("사용자 정보가 올바르지 않습니다. 다시 입력해주세요", "[ERROR] : 사용자 정보가 올바르지 않음"),
-	DEACTIVATED_USER("비활성화된 사용자입니다. 다시 확인해주세요", "[ERROR] : 탈퇴한 사용자"),
-	USER_ALREADY_EXISTS("이미 존재하는 사용자입니다.", "[ERROR] : 이미 존재하는 사용자");
+	DEACTIVATED_USER("비활성화된 사용자입니다. 다시 확인해주세요", "[ERROR] : 비활성화된 사용자"),
+	USER_ALREADY_EXISTS("이미 존재하는 사용자입니다.", "[ERROR] : 이미 존재하는 사용자"),
+	USER_ALREADY_DELETED("이미 삭제된 사용자입니다.", "[ERROR] : 이미 삭제된 사용자");
 
 	private final String message;
 	private final String logMessage;
@@ -18,7 +19,6 @@ public enum MemberErrorCode implements CustomErrorCode {
 		this.message = message;
 		this.logMessage = logMessage;
 	}
-
 
 	@Override
 	public String getLogMessage() {
