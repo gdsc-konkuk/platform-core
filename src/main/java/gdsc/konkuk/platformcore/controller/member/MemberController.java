2350688201a3a16389f5a0b58a5b9552b5760fc9
ekgns33 <ekgns00421@gmail.com>
2024-07-22 13:09:57 +0900

    feat : 회원 탈퇴 로직 구현

--- src/main/java/gdsc/konkuk/platformcore/controller/member/MemberController.java
+++ src/main/java/gdsc/konkuk/platformcore/controller/member/MemberController.java
@@ -2,13 +2,17 @@ package gdsc.konkuk.platformcore.controller.member;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import gdsc.konkuk.platformcore.application.member.MemberService;
+import gdsc.konkuk.platformcore.global.controller.SecurityUtils;
+import gdsc.konkuk.platformcore.global.responses.Response;
 import gdsc.konkuk.platformcore.global.responses.SuccessResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,9 +23,16 @@ public class MemberController {
 	private final MemberService memberService;
 
 	@PostMapping()
-	public ResponseEntity<SuccessResponse> signup(@RequestBody MemberRegisterRequest registerRequest) {
+	public ResponseEntity<Response> signup(@RequestBody @Valid MemberRegisterRequest registerRequest) {
 		memberService.register(registerRequest);
 		return ResponseEntity.status(HttpStatus.CREATED).body(SuccessResponse.messageOnly());
+	}
+
+	@DeleteMapping()
+	public ResponseEntity<Response> withdraw() {
+		Long currentId = SecurityUtils.getCurrentUserId();
+		memberService.withdraw(currentId);
+		return ResponseEntity.ok(SuccessResponse.messageOnly());
 	}
 
 }
