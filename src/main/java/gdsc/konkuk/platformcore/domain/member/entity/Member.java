9ed8da32f6c0ac6ce2a7e3a4040b32c65e4c4231
ekgns33 <ekgns00421@gmail.com>
2024-07-22 15:51:28 +0900

    feat : 식별자 전략 명시 IDENTITY

--- src/main/java/gdsc/konkuk/platformcore/domain/member/entity/Member.java
+++ src/main/java/gdsc/konkuk/platformcore/domain/member/entity/Member.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -27,7 +28,7 @@ import lombok.NoArgsConstructor;
 public class Member {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "member_id", unique = true)
