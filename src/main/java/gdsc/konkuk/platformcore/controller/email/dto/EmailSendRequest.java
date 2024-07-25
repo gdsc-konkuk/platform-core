6560599cb8e05139485438e5d1ac821e466d8115
ekgns33 <ekgns00421@gmail.com>
2024-07-25 18:48:27 +0900

    feat : Email전송 컨트롤러 작성

---
+++ src/main/java/gdsc/konkuk/platformcore/controller/email/dto/EmailSendRequest.java
@@ -0,0 +1,28 @@
+package gdsc.konkuk.platformcore.controller.email.dto;
+
+import java.util.List;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmailSendRequest {
+	@NotEmpty
+	private String subject;
+	@NotEmpty
+	private String content;
+	@NotNull
+	private List<@Email String> receivers;
+
+	@Builder
+	public EmailSendRequest(String subject, String content, List<String> receivers) {
+		this.subject = subject;
+		this.content = content;
+		this.receivers = receivers;
+	}
+}
