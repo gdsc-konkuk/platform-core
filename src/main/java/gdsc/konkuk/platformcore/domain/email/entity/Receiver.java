eea65a0ef3e9e2959b3d3400f0e1e1467f0725e4
ekgns33 <ekgns00421@gmail.com>
2024-07-25 23:52:47 +0900

    refactor : 불필요한 import 제거

--- src/main/java/gdsc/konkuk/platformcore/domain/email/entity/Receiver.java
+++ src/main/java/gdsc/konkuk/platformcore/domain/email/entity/Receiver.java
@@ -2,12 +2,9 @@ package gdsc.konkuk.platformcore.domain.email.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
