26a02ce8f8067112b97639e7562cadd1f1315d3b
ekgns33 <ekgns00421@gmail.com>
2024-07-25 18:50:36 +0900

    feat : 이메일 전송 클라이언트 설정, 전송 로직작성

---
+++ src/main/java/gdsc/konkuk/platformcore/external/email/exceptions/EmailErrorCode.java
@@ -0,0 +1,32 @@
+package gdsc.konkuk.platformcore.external.email.exceptions;
+
+import gdsc.konkuk.platformcore.global.exceptions.CustomErrorCode;
+
+public enum EmailErrorCode implements CustomErrorCode {
+
+	MAIL_PARSING_ERROR("메일 전송을 다시 시도해 주세요", "메일 파싱에 실패했습니다."),
+	MAIL_SENDING_ERROR("메일 전송을 다시 시도해 주세요", "메일 전송에 실패했습니다."),;
+
+	private final String message;
+	private final String logMessage;
+
+	EmailErrorCode(String message, String logMessage) {
+		this.message = message;
+		this.logMessage = logMessage;
+	}
+
+	@Override
+	public String getLogMessage() {
+		return logMessage;
+	}
+
+	@Override
+	public String getName() {
+		return name();
+	}
+
+	@Override
+	public String getMessage() {
+		return message;
+	}
+}
