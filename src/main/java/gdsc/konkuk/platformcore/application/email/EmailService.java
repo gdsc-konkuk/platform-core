9610a2290764f049ea6704bc07a271833f0cc86f
ekgns33 <ekgns00421@gmail.com>
2024-07-25 18:48:59 +0900

    feat : 이메일서비스 작성

---
+++ src/main/java/gdsc/konkuk/platformcore/application/email/EmailService.java
@@ -0,0 +1,30 @@
+package gdsc.konkuk.platformcore.application.email;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import gdsc.konkuk.platformcore.controller.email.dto.EmailSendRequest;
+import gdsc.konkuk.platformcore.domain.email.entity.Email;
+import gdsc.konkuk.platformcore.domain.email.repository.EmailRepository;
+import gdsc.konkuk.platformcore.external.email.EmailClient;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService{
+
+	private final EmailClient emailClient;
+	private final EmailRepository emailRepository;
+
+	@Transactional
+	public Long process(EmailSendRequest request) {
+		Email email = Email.builder()
+			.subject(request.getSubject())
+			.content(request.getContent())
+			.build();
+		email.addReceivers(request.getReceivers());
+		emailRepository.save(email);
+		emailClient.sendAll(email);
+		return email.getId();
+	}
+}
