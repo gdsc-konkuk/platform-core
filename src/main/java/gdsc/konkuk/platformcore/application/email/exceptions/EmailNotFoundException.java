901b90571752d256b5367f64cc460c5014c4622f
ekgns33 <ekgns00421@gmail.com>
2024-07-29 14:32:47 +0900

    feature : 이메일 작업등록 관련 에러, 에러코드 추가

---
+++ src/main/java/gdsc/konkuk/platformcore/application/email/exceptions/EmailNotFoundException.java
@@ -0,0 +1,15 @@
+package gdsc.konkuk.platformcore.application.email.exceptions;
+
+import gdsc.konkuk.platformcore.global.exceptions.BusinessException;
+import gdsc.konkuk.platformcore.global.exceptions.CustomErrorCode;
+
+public class EmailNotFoundException extends BusinessException {
+
+  protected EmailNotFoundException(CustomErrorCode errorCode, String logMessage) {
+    super(errorCode, logMessage);
+  }
+
+  public static EmailNotFoundException of(CustomErrorCode errorCode) {
+    return new EmailNotFoundException(errorCode, errorCode.getMessage());
+  }
+}
