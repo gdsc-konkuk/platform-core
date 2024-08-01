fb29f33359613507259316ec1b089f66df238af4
WhitePiano <startoflateness@gmail.com>
2024-08-01 14:54:44 +0900

    Bp 38 retrospect crud api implementation (#12)
    * feat: 이벤트 및 회고 등록 기능
    
    * feat: 이벤트 등록 controller 작성 및 테스트
    
    * feat: 이벤트 회고 조회 기능
    
    * feat: 이벤트 회고 조회 controller & 테스트
    
    * feat: 이벤트 회고 수정 기능
    
    * feat: 이벤트 회고 수정 controller 및 test
    
    * refactor: `Retrospect`를 `Event`의 Embedding으로 변경
    
    * fix: `@Valid`를 통한 dto 검증 추가

--- src/main/java/gdsc/konkuk/platformcore/application/event/EventService.java
+++ src/main/java/gdsc/konkuk/platformcore/application/event/EventService.java
@@ -1,6 +1,11 @@
 package gdsc.konkuk.platformcore.application.event;
 
+import gdsc.konkuk.platformcore.application.event.exceptions.EventErrorCode;
+import gdsc.konkuk.platformcore.application.event.exceptions.EventNotFoundException;
+import gdsc.konkuk.platformcore.controller.event.EventRegisterRequest;
+import gdsc.konkuk.platformcore.domain.event.entity.Event;
 import gdsc.konkuk.platformcore.domain.event.repository.EventRepository;
+import gdsc.konkuk.platformcore.domain.event.entity.Retrospect;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
@@ -16,9 +21,32 @@ import java.util.List;
 public class EventService {
   private final EventRepository eventRepository;
 
+  @Transactional
+  public Event register(EventRegisterRequest registerRequest) {
+    Event newEvent = EventRegisterRequest.toEntity(registerRequest);
+    return eventRepository.save(newEvent);
+  }
+
   public List<EventWithAttendance> getEventsOfTheMonthWithAttendance(LocalDate month) {
     return eventRepository.findAllWithAttendanceByStartAtBetween(
-      month.withDayOfMonth(1).atStartOfDay(),
-      month.withDayOfMonth(month.lengthOfMonth()).atTime(LocalTime.MAX));
+        month.withDayOfMonth(1).atStartOfDay(),
+        month.withDayOfMonth(month.lengthOfMonth()).atTime(LocalTime.MAX));
+  }
+
+  public Retrospect getRetrospect(Long eventId) {
+    Event event =
+        eventRepository
+            .findById(eventId)
+            .orElseThrow(() -> EventNotFoundException.of(EventErrorCode.EVENT_NOT_FOUND));
+    return event.getRetrospect();
+  }
+
+  @Transactional
+  public void updateRetrospect(Long eventId, String content) {
+    Event event =
+        eventRepository
+            .findById(eventId)
+            .orElseThrow(() -> EventNotFoundException.of(EventErrorCode.EVENT_NOT_FOUND));
+    event.updateRetrospectContent(content);
   }
 }
---
+++ src/main/java/gdsc/konkuk/platformcore/controller/event/RetrospectUpdateRequest.java
@@ -0,0 +1,17 @@
+package gdsc.konkuk.platformcore.controller.event;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RetrospectUpdateRequest {
+  @NotEmpty private String content;
+}
