46f15f0a7653695643b1f461539835e44e4be19c
ekgns33 <ekgns00421@gmail.com>
2024-08-02 18:17:50 +0900

    feat : 작업 정보 메모리 저장소 작성

---
+++ src/main/java/gdsc/konkuk/platformcore/global/scheduler/TaskInMemoryRepository.java
@@ -0,0 +1,45 @@
+package gdsc.konkuk.platformcore.global.scheduler;
+
+import gdsc.konkuk.platformcore.global.exceptions.GlobalErrorCode;
+import gdsc.konkuk.platformcore.global.exceptions.TaskNotFoundException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ScheduledFuture;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskInMemoryRepository {
+
+  private final ConcurrentMap<String, ScheduledFuture<?>> taskMap = new ConcurrentHashMap<>();
+
+  public void addTask(String taskId, ScheduledFuture<?> future) {
+    taskMap.put(taskId, future);
+  }
+
+  public ScheduledFuture<?> getTask(String taskId) {
+
+    ScheduledFuture<?> future = taskMap.get(taskId);
+    if(future == null) {
+      throw TaskNotFoundException.of(GlobalErrorCode.SCHEDULED_TASK_NOT_FOUND);
+    }
+    return future;
+  }
+
+  public int size() {
+    return taskMap.size();
+  }
+
+  public void removeAll() {
+    for(ScheduledFuture<?> task : taskMap.values()) {
+      task.cancel(true);
+    }
+    taskMap.clear();
+  }
+
+  public void removeTask(String taskId) {
+    ScheduledFuture<?> task = taskMap.remove(taskId);
+    if(task == null) {
+      throw TaskNotFoundException.of(GlobalErrorCode.SCHEDULED_TASK_NOT_FOUND);
+    }
+  }
+}
