cd8acf7529acf1cf965fa9783de4da2663942254
ekgns33 <ekgns00421@gmail.com>
2024-08-05 15:06:27 +0900

    refactor : 함수 네이밍 변경

--- src/main/java/gdsc/konkuk/platformcore/application/email/EmailTaskFacade.java
+++ src/main/java/gdsc/konkuk/platformcore/application/email/EmailTaskFacade.java
@@ -7,28 +7,30 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class EmailScheduleService {
+@Transactional
+public class EmailTaskFacade {
 
   private final EmailService emailService;
   private final TaskScheduler emailTaskScheduler;
 
-  public EmailTask scheduleEmailTask(EmailSendRequest request) {
+  public EmailTask register(EmailSendRequest request) {
     EmailTask emailTask = EmailSendRequest.toEntity(request);
     emailService.registerTask(emailTask);
     emailTaskScheduler.scheduleSyncTask(emailTask, getWaitingPeriod(emailTask));
     return emailTask;
   }
 
-  public void reScheduleEmailTask(Long emailId, EmailSendRequest request) {
+  public void update(Long emailId, EmailSendRequest request) {
     emailTaskScheduler.cancelTask(String.valueOf(emailId));
     EmailTask updatedTask = emailService.update(emailId, request);
     emailTaskScheduler.scheduleSyncTask(updatedTask, getWaitingPeriod(updatedTask));
   }
 
-  public void cancelEmailTask(Long emailId) {
+  public void cancel(Long emailId) {
     emailTaskScheduler.cancelTask(String.valueOf(emailId));
     emailService.delete(emailId);
   }
