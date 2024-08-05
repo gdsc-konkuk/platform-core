06909a06419fda1e5c52742a69d56e722dfcdf28
ekgns33 <ekgns00421@gmail.com>
2024-08-05 16:11:29 +0900

    test : 디스코드 메세지 테스트 작성

---
+++ src/test/java/gdsc/konkuk/platformcore/external/discord/DiscordMessageTest.java
@@ -0,0 +1,27 @@
+package gdsc.konkuk.platformcore.external.discord;
+
+import static gdsc.konkuk.platformcore.global.consts.PlatformConstants.DISCORD_ERROR_TITLE;
+import static org.junit.jupiter.api.Assertions.*;
+
+import gdsc.konkuk.platformcore.application.email.exceptions.EmailErrorCode;
+import gdsc.konkuk.platformcore.external.email.exceptions.EmailSendingException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DiscordMessageTest {
+
+  @Test
+  @DisplayName("디스코드 에러메세지 생성 시 서버 로그 메세지를 출력")
+  void should_create_discord_error_message_with_server_log_message() {
+    // given
+    Exception occurredException = EmailSendingException.of(EmailErrorCode.EMAIL_NOT_FOUND);
+
+    // when
+    DiscordMessage discordMessage = DiscordMessage.of(occurredException);
+
+    // then
+    assertEquals(DISCORD_ERROR_TITLE, discordMessage.getContent());
+    assertEquals(occurredException.getMessage(), discordMessage.getEmbeds().get(0).getTitle());
+  }
+
+}
