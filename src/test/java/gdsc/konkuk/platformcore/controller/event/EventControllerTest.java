7b9314f590a44bbf78890c318b9cb571a2315a18
WhitePiano <startoflateness@gmail.com>
2024-08-07 14:05:55 +0900

    test: 이벤트 CRU 테스트

--- src/test/java/gdsc/konkuk/platformcore/controller/event/EventControllerTest.java
+++ src/test/java/gdsc/konkuk/platformcore/controller/event/EventControllerTest.java
@@ -3,10 +3,14 @@ package gdsc.konkuk.platformcore.controller.event;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import gdsc.konkuk.platformcore.application.event.EventBrief;
 import gdsc.konkuk.platformcore.application.event.EventService;
 
 import gdsc.konkuk.platformcore.domain.event.entity.Event;
 import gdsc.konkuk.platformcore.domain.event.entity.Retrospect;
+import gdsc.konkuk.platformcore.external.s3.StorageObject;
+import java.net.URL;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,16 +19,19 @@ import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDateTime;
+import org.springframework.web.multipart.MultipartFile;
 
 import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
@@ -34,7 +41,6 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.MockitoAnnotations.openMocks;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
@@ -72,6 +78,128 @@ public class EventControllerTest {
   }
 
   @Test
+  @DisplayName("모든 이벤트를 간략 조회할 수 있다")
+  @WithMockUser
+  void should_get_all_events_when_request() throws Exception {
+    // given
+    given(eventService.getAllBriefs())
+        .willReturn(
+            EventBriefResponse.builder()
+                .eventBriefs(
+                    List.of(
+                        EventBrief.builder()
+                            .id(1L)
+                            .title("test event1")
+                            .content("test event content")
+                            .startAt(LocalDateTime.now())
+                            .thumbnail(new URL("https://foo.com/bar/baz.jpg"))
+                            .build(),
+                        EventBrief.builder()
+                            .id(2L)
+                            .title("test event2")
+                            .content("test event content 2")
+                            .startAt(LocalDateTime.now())
+                            .thumbnail(new URL("https://foo.com/bar/baz.jpg"))
+                            .build(),
+                        EventBrief.builder()
+                            .id(3L)
+                            .title("test event3")
+                            .content("test event content")
+                            .startAt(LocalDateTime.now())
+                            .thumbnail(new URL("https://foo.com/bar/baz.jpg"))
+                            .build()))
+                .build());
+
+    // when
+    ResultActions result =
+        mockMvc.perform(RestDocumentationRequestBuilders.get("/api/v1/events").with(csrf()));
+
+    // then
+    result
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andDo(
+            document(
+                "getAllEvents",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .description("모든 이벤트를 조회할 수 있다")
+                        .tag("events")
+                        .responseFields(
+                            fieldWithPath("success").description("성공 여부"),
+                            fieldWithPath("message").description("메시지"),
+                            fieldWithPath("data.eventBriefs[].id").description("이벤트 ID"),
+                            fieldWithPath("data.eventBriefs[].title").description("이벤트 제목"),
+                            fieldWithPath("data.eventBriefs[].content").description("이벤트 내용"),
+                            fieldWithPath("data.eventBriefs[].startAt").description("이벤트 시작 시간"),
+                            fieldWithPath("data.eventBriefs[].thumbnail").description("썸네일 URL"))
+                        .build())));
+  }
+
+  @Test
+  @DisplayName("특정 이벤트를 상세 조회할 수 있다")
+  @WithMockUser
+  void should_get_event_detail_when_pass_event_id() throws Exception {
+    // given
+    given(eventService.getEvent(any(Long.class)))
+        .willReturn(
+            EventDetailResponse.builder()
+                .id(1L)
+                .title("test title")
+                .content("test content")
+                .startAt(LocalDateTime.now())
+                .endAt(LocalDateTime.now().plusHours(2))
+                .images(
+                    List.of(
+                        StorageObject.builder()
+                            .key("asdfasdf")
+                            .url(new URL("https://foo.com/bar/baz1.jpg"))
+                            .build(),
+                        StorageObject.builder()
+                            .key("asdffdsa")
+                            .url(new URL("https://foo.com/bar/baz2.jpg"))
+                            .build(),
+                        StorageObject.builder()
+                            .key("asdffffff")
+                            .url(new URL("https://foo.com/bar/baz3.png"))
+                            .build()))
+                .build());
+
+    // when
+    ResultActions result =
+        mockMvc.perform(
+            RestDocumentationRequestBuilders.get("/api/v1/events/{eventId}", 1L).with(csrf()));
+
+    // then
+    result
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andDo(
+            document(
+                "getEvent",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .description("특정 이벤트를 상세 조회할 수 있다")
+                        .tag("events")
+                        .pathParameters(parameterWithName("eventId").description("이벤트 ID"))
+                        .responseFields(
+                            fieldWithPath("success").description("성공 여부"),
+                            fieldWithPath("message").description("메시지"),
+                            fieldWithPath("data.id").description("이벤트 ID"),
+                            fieldWithPath("data.title").description("이벤트 제목"),
+                            fieldWithPath("data.content").description("이벤트 내용"),
+                            fieldWithPath("data.startAt").description("이벤트 시작 시간"),
+                            fieldWithPath("data.endAt").description("이벤트 종료 시간"),
+                            fieldWithPath("data.images[].key").description("이미지 Key"),
+                            fieldWithPath("data.images[].url").description("이미지 URL"))
+                        .build())));
+  }
+
+  @Test
   @DisplayName("이벤트를 등록할 수 있다")
   @WithMockUser
   void should_register_event_when_requested() throws Exception {
@@ -83,15 +211,28 @@ public class EventControllerTest {
             .startAt(LocalDateTime.now())
             .endAt(LocalDateTime.now().plusHours(2))
             .build();
+    MockMultipartFile mockImages =
+        new MockMultipartFile("images", "test.jpg", "image/jpeg", "test".getBytes());
+    MockMultipartFile mockDetail =
+        new MockMultipartFile(
+            "detail",
+            "",
+            "application/json",
+            objectMapper.writeValueAsString(eventRegisterRequest).getBytes());
     given(eventService.register(any(), any())).willReturn(this.event);
 
     // when
     ResultActions result =
         mockMvc.perform(
-            RestDocumentationRequestBuilders.post("/api/v1/events")
-                .contentType(MULTIPART_FORM_DATA)
-                .formField("imageFiles", "test")
-                .formField("registerRequest", objectMapper.writeValueAsString(eventRegisterRequest))
+            RestDocumentationRequestBuilders.multipart("/api/v1/events")
+                .file(mockImages)
+                .file(mockDetail)
+                .content(
+                    objectMapper.writeValueAsString(
+                        new Object() {
+                          public Object detail = eventRegisterRequest;
+                          public List<MultipartFile> images = List.of();
+                        }))
                 .with(csrf()));
 
     // then
@@ -109,11 +250,106 @@ public class EventControllerTest {
                         .tag("events")
                         .responseHeaders(headerWithName("Location").description("등록한 Event URI"))
                         .requestFields(
-                            fieldWithPath("title").description("이벤트 제목"),
-                            fieldWithPath("description").description("이벤트 설명"),
-                            fieldWithPath("thumbnailUrl").description("썸네일 URL"),
-                            fieldWithPath("startAt").description("이벤트 시작 시간"),
-                            fieldWithPath("endAt").description("이벤트 종료 시간"))
+                            fieldWithPath("detail")
+                                .description(
+                                    """
+                                    이벤트 정보
+                                    - title(String): 이벤트 제목
+                                    - content(String): 이벤트 내용
+                                    - startAt(DateTime): 이벤트 시작 시간
+                                    - endAt(DateTime): 이벤트 종료 시간
+                                    """),
+                            fieldWithPath("detail.title").description("이벤트 제목"),
+                            fieldWithPath("detail.content").description("이벤트 내용"),
+                            fieldWithPath("detail.startAt").description("이벤트 시작 시간"),
+                            fieldWithPath("detail.endAt").description("이벤트 종료 시간"),
+                            fieldWithPath("images[]").description("이미지 파일 목록 (여러개)").optional())
+                        .responseFields(
+                            fieldWithPath("success").description("성공 여부"),
+                            fieldWithPath("message").description("메시지"),
+                            fieldWithPath("data").description("null"))
+                        .build())));
+  }
+
+  @Test
+  @DisplayName("이벤트를 수정할 수 있다")
+  @WithMockUser
+  void should_update_event_when_requested() throws Exception {
+    // given
+    EventUpdateRequest eventUpdateRequest =
+        EventUpdateRequest.builder()
+            .title("test title")
+            .content("test description")
+            .startAt(LocalDateTime.now())
+            .endAt(LocalDateTime.now().plusHours(2))
+            .eventImageKeysToDelete(List.of("key1", "key2"))
+            .build();
+    MockMultipartFile mockImages =
+        new MockMultipartFile("new-images", "test.jpg", "image/jpeg", "test".getBytes());
+    MockMultipartFile mockDetail =
+        new MockMultipartFile(
+            "detail",
+            "",
+            "application/json",
+            objectMapper.writeValueAsString(eventUpdateRequest).getBytes());
+    doNothing().when(eventService).update(any(Long.class), any(EventUpdateRequest.class), any());
+
+    // when
+    MockMultipartHttpServletRequestBuilder putMultipartRestDocumentationRequestBuilder =
+        RestDocumentationRequestBuilders.multipart("/api/v1/events/{eventId}", 1L);
+    putMultipartRestDocumentationRequestBuilder.with(
+        request -> {
+          request.setMethod("PATCH");
+          return request;
+        });
+    ResultActions result =
+        mockMvc.perform(
+            putMultipartRestDocumentationRequestBuilder
+                .file(mockImages)
+                .file(mockDetail)
+                .content(
+                    objectMapper.writeValueAsString(
+                        new Object() {
+                          public Object detail = eventUpdateRequest;
+                          public List<MultipartFile> newImages = List.of();
+                        }))
+                .with(csrf()));
+
+    // then
+    result
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andDo(
+            document(
+                "updateEvent",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                resource(
+                    ResourceSnippetParameters.builder()
+                        .description("이벤트를 수정할 수 있다")
+                        .tag("events")
+                        .pathParameters(parameterWithName("eventId").description("이벤트 ID"))
+                        .requestFields(
+                            fieldWithPath("newImages[]")
+                                .description(
+                                    "**주의! form field 이름은 `new-images`입니다.**\n새로 추가할 이미지 파일 목록 (여러개)")
+                                .optional(),
+                            fieldWithPath("detail")
+                                .description(
+                                    """
+                                    이벤트 정보
+                                    - title(String?): 이벤트 제목
+                                    - content(String?): 이벤트 내용
+                                    - startAt(DateTime?): 이벤트 시작 시간
+                                    - endAt(DateTime?): 이벤트 종료 시간
+                                    - eventImageKeysToDelete(String[]?): 삭제할 이미지 Key 목록
+                                    """),
+                            fieldWithPath("detail.title").description("이벤트 제목"),
+                            fieldWithPath("detail.content").description("이벤트 내용"),
+                            fieldWithPath("detail.startAt").description("이벤트 시작 시간"),
+                            fieldWithPath("detail.endAt").description("이벤트 종료 시간"),
+                            fieldWithPath("detail.eventImageKeysToDelete[]")
+                                .description("삭제할 이미지 Key 목록"))
                         .responseFields(
                             fieldWithPath("success").description("성공 여부"),
                             fieldWithPath("message").description("메시지"),
