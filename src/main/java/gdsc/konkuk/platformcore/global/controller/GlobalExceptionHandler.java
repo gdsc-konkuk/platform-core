0f96069ea33b5707469c23c85b659daaad16b4ae
WhitePiano <startoflateness@gmail.com>
2024-08-09 22:09:11 +0900

    fix: `BusinessException`을 response 할 때, name 대신 `logMessage`를 반환하던 문제 해결

--- src/main/java/gdsc/konkuk/platformcore/global/controller/GlobalExceptionHandler.java
+++ src/main/java/gdsc/konkuk/platformcore/global/controller/GlobalExceptionHandler.java
@@ -18,7 +18,7 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(BusinessException.class)
   protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
     log.error("BusinessException Caught! [{}]", e.getLogMessage());
-    final ErrorResponse response = ErrorResponse.of(e.getMessage(), e.getLogMessage());
+    final ErrorResponse response = ErrorResponse.of(e.getMessage(), e.getName());
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 
