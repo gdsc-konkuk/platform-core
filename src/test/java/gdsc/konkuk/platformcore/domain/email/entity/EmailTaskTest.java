533419219c64d88565ecf76f581d4be2a4a91b10
ekgns33 <ekgns00421@gmail.com>
2024-08-11 19:41:48 +0900

    test: 이메일 작업 전송대상 업데이트 필터링 테스트

--- src/test/java/gdsc/konkuk/platformcore/domain/email/entity/EmailTaskTest.java
+++ src/test/java/gdsc/konkuk/platformcore/domain/email/entity/EmailTaskTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,13 +20,14 @@ class EmailTaskTest {
               .subject("예시 이메일 제목")
               .content("Html 문자열")
               .build())
-          .receivers(new EmailReceivers(List.of("example@gmail.com", "example3@gmail.com")))
+          .receivers(new EmailReceivers(Set.of("example@gmail.com", "example3@gmail.com")))
           .sendAt(LocalDateTime.of(2021, 10, 10, 10, 10))
           .build();
-      EmailReceivers newReceivers = new EmailReceivers(List.of("aaa@gmail.com", "bbb@gmail.com", "ccc@gmail.com"));
+      EmailReceivers newReceivers = new EmailReceivers(
+          Set.of("aaa@gmail.com", "bbb@gmail.com", "ccc@gmail.com"));
       //when
 
-      emailTask.changeEmailReceivers(newReceivers);
+      emailTask.changeEmailReceivers(newReceivers.getReceivers());
 
       //then
       assertEquals(newReceivers, emailTask.getEmailReceivers());
@@ -38,7 +40,7 @@ class EmailTaskTest {
     EmailTask emailTask =
         EmailTask.builder()
             .emailDetails(EmailDetails.builder().subject("예시 이메일 제목").content("Html 문자열").build())
-            .receivers(new EmailReceivers(List.of()))
+            .receivers(new EmailReceivers(Set.of()))
             .sendAt(LocalDateTime.of(2021, 10, 10, 10, 10))
             .build();
 
@@ -59,7 +61,7 @@ class EmailTaskTest {
     EmailTask emailTask =
         EmailTask.builder()
             .emailDetails(EmailDetails.builder().subject("예시 이메일 제목").content("Html 문자열").build())
-            .receivers(new EmailReceivers(List.of()))
+            .receivers(new EmailReceivers(Set.of()))
             .sendAt(LocalDateTime.of(2021, 10, 10, 10, 10))
             .build();
 
@@ -68,5 +70,41 @@ class EmailTaskTest {
 
     // then
     assertTrue(emailTask.isSent());
+  }
+
+  @Test
+  @DisplayName("수정 전 전송대상 중 겹치는 대상 추출")
+  void should_success_when_get_overlapping_receivers() {
+    // given
+    EmailTask emailTask =
+        EmailTask.builder()
+            .emailDetails(EmailDetails.builder().subject("예시 이메일 제목").content("Html 문자열").build())
+            .receivers(new EmailReceivers(Set.of("example1.com", "example2.com")))
+            .sendAt(LocalDateTime.of(2021, 10, 10, 10, 10))
+            .build();
+    Set<String> newEmailReceivers = Set.of("example2.com", "example3.com");
+    // when
+    List<String> expected = List.of("example2.com");
+    List<String> actual = emailTask.filterReceiversInPrevSet(newEmailReceivers);
+    // then
+    assertEquals(actual, expected);
+  }
+
+  @Test
+  @DisplayName("수정된 전송 대상 중 이전과 겹치지 않는 대상 추출")
+  void should_success_when_get_receivers_not_in_prev_set() {
+    // given
+    EmailTask emailTask =
+        EmailTask.builder()
+            .emailDetails(EmailDetails.builder().subject("예시 이메일 제목").content("Html 문자열").build())
+            .receivers(new EmailReceivers(Set.of("example1.com", "example2.com")))
+            .sendAt(LocalDateTime.of(2021, 10, 10, 10, 10))
+            .build();
+    Set<String> newEmailReceivers = Set.of("example2.com", "example3.com");
+    // when
+    List<String> expected = List.of("example3.com");
+    List<String> actual = emailTask.filterReceiversNotInPrevSet(newEmailReceivers);
+    // then
+    assertEquals(actual, expected);
   }
 }
