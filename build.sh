547a80ec718dd541adb0387c088e0cde00af6fc6
ekgns33 <ekgns00421@gmail.com>
2024-08-16 11:26:03 +0900

    chore : 빌드파일 작성

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 프로젝트 클린 및 빌드
+./gradlew clean bootJar
+
+# Docker 이미지 빌드
+docker build -t ekgns33/gdsc-spring:latest .
+
+# Docker 이미지 푸시
+docker push ekgns33/gdsc-spring:latest
+
+# 완료 메시지
+echo "Docker image pushed to ekgns33/gdsc-spring:latest successfully."
