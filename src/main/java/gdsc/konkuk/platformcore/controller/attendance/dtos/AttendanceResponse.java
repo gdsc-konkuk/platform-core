62000d5eb9af00e2eaa9743109e1fd71fe491b5f
WhitePiano <startoflateness@gmail.com>
2024-08-20 18:10:30 +0900

    change: 출석 및 출석 URL 생성 응답에 body 추가

---
+++ src/main/java/gdsc/konkuk/platformcore/controller/attendance/dtos/AttendanceResponse.java
@@ -0,0 +1,22 @@
+package gdsc.konkuk.platformcore.controller.attendance.dtos;
+
+import gdsc.konkuk.platformcore.domain.attendance.entity.Attendance;
+import java.net.URI;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AttendanceResponse {
+  private Long attendanceId;
+  private URI attendUrl;
+
+  public static AttendanceResponse from(Attendance attendance, URI attendUrl) {
+    return AttendanceResponse.builder()
+        .attendanceId(attendance.getId())
+        .attendUrl(attendUrl)
+        .build();
+  }
+}
