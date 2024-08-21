20b453de0ca05935239a8f2f9b01879d0d7246d5
WhitePiano <startoflateness@gmail.com>
2024-08-21 19:09:45 +0900

    Feat: request attendance state (#37)
    * feat: Attendance Status 조회 API
    
    * test: Attendance Status 조회 API Test

---
+++ src/main/java/gdsc/konkuk/platformcore/application/attendance/dtos/AttendanceStatus.java
@@ -0,0 +1,22 @@
+package gdsc.konkuk.platformcore.application.attendance.dtos;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AttendanceStatus {
+  private Long attendanceId;
+  private int total;
+  private int attended;
+
+  public static AttendanceStatus of(Long attendanceId, int total, int attended) {
+    return AttendanceStatus.builder()
+        .attendanceId(attendanceId)
+        .total(total)
+        .attended(attended)
+        .build();
+  }
+}
