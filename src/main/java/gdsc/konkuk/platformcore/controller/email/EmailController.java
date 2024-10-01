0244ed8fbbba8c67c46c5f640491cd4fb33f3168
ekgns33 <ekgns00421@gmail.com>
2024-10-02 03:28:08 +0900

    feat : 페이지네이션 가능한 엔드포인트작성

--- src/main/java/gdsc/konkuk/platformcore/controller/email/EmailController.java
+++ src/main/java/gdsc/konkuk/platformcore/controller/email/EmailController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,6 +33,14 @@ public class EmailController {
   @GetMapping
   public ResponseEntity<SuccessResponse> getAllEmailTask() {
     EmailTaskListResponse emailTasks = EmailTaskMapper.mapToEmailTaskListResponse(emailService.getAllTaskAsList());
+    return ResponseEntity.ok(SuccessResponse.of(emailTasks));
+  }
+
+  @GetMapping("/pages")
+  public ResponseEntity<SuccessResponse> getAllEmailTask(
+      @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo
+  ) {
+    EmailTaskListResponse emailTasks = EmailTaskMapper.mapToEmailTaskPageResponse(emailService.getAllTaskWithPage(pageNo));
     return ResponseEntity.ok(SuccessResponse.of(emailTasks));
   }
 
