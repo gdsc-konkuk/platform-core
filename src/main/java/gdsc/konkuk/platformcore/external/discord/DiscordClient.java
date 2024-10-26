523b1f5019a95a8fa281e780f3d08b04523a337b
ekgns33 <ekgns00421@gmail.com>
2024-10-26 17:05:22 +0900

    fix : `URI.create`에서 `UriComponentsBuilder`로 전환

--- src/main/java/gdsc/konkuk/platformcore/external/discord/DiscordClient.java
+++ src/main/java/gdsc/konkuk/platformcore/external/discord/DiscordClient.java
@@ -1,6 +1,5 @@
 package gdsc.konkuk.platformcore.external.discord;
 
-import java.net.URI;
 import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
 @Component
@@ -21,14 +21,20 @@ public class DiscordClient {
 
   private final RestTemplate restTemplate;
 
+  /***
+   * Discord Webhook을 통해 에러메세지를 전송한다.
+   * @param e 발생한 Exception
+   * @author ekgns33
+   */
   public void sendErrorMessage(Exception e) {
     DiscordMessage message = DiscordMessage.of(e);
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
     HttpEntity<DiscordMessage> entity = new HttpEntity<>(message, headers);
     try {
-      restTemplate.postForObject(URI.create(WEB_HOOK_URL), entity, String.class);
+      restTemplate.postForObject(UriComponentsBuilder.fromUriString(WEB_HOOK_URL).toUriString(), entity, String.class);
     } catch (Exception ex) {
+      //TODO: 웹훅 요청 자체가 잘못된 경우에 대한 처리 고안해야함.
       log.error(Arrays.toString(ex.getStackTrace()));
     }
   }
