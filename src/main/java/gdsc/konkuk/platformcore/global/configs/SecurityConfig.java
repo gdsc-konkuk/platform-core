c36f0bdb31e68cee0a29e257c51f11367f688aac
WhitePiano <startoflateness@gmail.com>
2024-12-25 18:31:34 +0900

    refactor(security): change oauth2 login redirection url more flexible
    - use `{baseUrl}`
    - enable `X-forwarded-*`

--- src/main/java/gdsc/konkuk/platformcore/global/configs/SecurityConfig.java
+++ src/main/java/gdsc/konkuk/platformcore/global/configs/SecurityConfig.java
@@ -105,8 +105,7 @@ public class SecurityConfig {
                 .clientSecret(googleOidcConfig.getClientSecret())
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri(
-                        "https://{baseHost}{basePort}{basePath}/login/oauth2/code/{registrationId}")
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
                 .scope("openid", "profile", "email")
                 .authorizationUri("https://accounts.google.com/o/oauth2/v2/auth")
                 .tokenUri("https://www.googleapis.com/oauth2/v4/token")
