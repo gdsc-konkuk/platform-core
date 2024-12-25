d1850f1cdbc679f66e999d755ba83176c26f5e2f
WhitePiano <startoflateness@gmail.com>
2024-12-25 18:32:59 +0900

    feat(chore): update docker image owner/name

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 ./gradlew clean bootJar -PexcludeSecrets=true
 
 # Docker 이미지 빌드 & 푸시
-docker buildx build --platform linux/amd64,linux/arm64 -t ekgns33/gdsc-spring:latest . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t goldentrash/gdsc-internal:latest . --push
 
 # 완료 메시지
-echo "Docker image pushed to ekgns33/gdsc-spring:latest successfully."
+echo "Docker image pushed to goldentrash/gdsc-internal:latest successfully."
