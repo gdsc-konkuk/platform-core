d1d43683b23ca2cc0e2dc8275baf2224e4c8ede5
WhitePiano <startoflateness@gmail.com>
2024-12-31 17:18:25 +0900

    fix(auth): set redirection to SPA

--- src/main/java/gdsc/konkuk/platformcore/application/auth/CustomAuthenticationSuccessHandler.java
+++ src/main/java/gdsc/konkuk/platformcore/application/auth/CustomAuthenticationSuccessHandler.java
@@ -6,6 +6,7 @@ import gdsc.konkuk.platformcore.domain.member.entity.Member;
 import gdsc.konkuk.platformcore.domain.member.repository.MemberRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -24,7 +25,7 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Override
     public void onAuthenticationSuccess(
             HttpServletRequest request, HttpServletResponse response,
-            Authentication authentication) {
+            Authentication authentication) throws IOException {
 
         OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
         Member member = memberRepository.findByEmail(oidcUser.getEmail())
@@ -34,5 +35,6 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         response.addHeader("Authorization", "Bearer " + token);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.sendRedirect("https://admin.gdsc-konkuk.dev/oauth/callback");
     }
 }
