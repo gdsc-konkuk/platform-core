ff111634700f1208d4a011038fe937ef01b10cec
WhitePiano <startoflateness@gmail.com>
2025-02-26 13:44:24 +0900

    feat: log when auth failed

--- src/main/java/gdsc/konkuk/platformcore/filter/auth/CustomAuthenticationFailureHandler.java
+++ src/main/java/gdsc/konkuk/platformcore/filter/auth/CustomAuthenticationFailureHandler.java
@@ -1,20 +1,19 @@
 package gdsc.konkuk.platformcore.filter.auth;
 
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gdsc.konkuk.platformcore.application.member.exceptions.MemberErrorCode;
 import gdsc.konkuk.platformcore.global.exceptions.CustomErrorCode;
-import gdsc.konkuk.platformcore.global.exceptions.GlobalErrorCode;
 import gdsc.konkuk.platformcore.global.responses.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
@@ -25,10 +24,8 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException exception) throws
             IOException {
-
-        CustomErrorCode errorCode =
-                request.getContentType().startsWith(APPLICATION_FORM_URLENCODED_VALUE)
-                        ? MemberErrorCode.INVALID_USER_INFO : GlobalErrorCode.ARGUMENT_NOT_VALID;
+        log.info("Authentication failed: {}", exception.getMessage());
+        CustomErrorCode errorCode = MemberErrorCode.INVALID_USER_INFO;
         ErrorResponse errorResponse = ErrorResponse.of(errorCode);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
