3ec1c8b85d76cb7388d8c46026affb3af67c287a
WhitePiano <startoflateness@gmail.com>
2025-02-27 15:44:42 +0900

    chore: apply cors configuration (#75)

--- src/main/java/gdsc/konkuk/platformcore/global/configs/SecurityConfig.java
+++ src/main/java/gdsc/konkuk/platformcore/global/configs/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +31,7 @@ public class SecurityConfig {
     private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
     private final CustomOAuthUserService customOAuthUserService;
     private final GoogleOidcConfig googleOidcConfig;
+    private final UrlBasedCorsConfigurationSource corsConfigurationSource;
 
     @Bean
     @Order(2)
@@ -40,6 +42,7 @@ public class SecurityConfig {
                 response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
         httpSecurity
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
