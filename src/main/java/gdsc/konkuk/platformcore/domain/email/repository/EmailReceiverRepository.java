943048396bc975d9dd856cd7e6aab292abb2fb9b
ekgns33 <ekgns00421@gmail.com>
2025-06-28 21:49:53 +0900

    fix : fix interface declaration

--- src/main/java/gdsc/konkuk/platformcore/domain/email/repository/EmailReceiverRepository.java
+++ src/main/java/gdsc/konkuk/platformcore/domain/email/repository/EmailReceiverRepository.java
@@ -3,6 +3,5 @@ package gdsc.konkuk.platformcore.domain.email.repository;
 import gdsc.konkuk.platformcore.domain.email.entity.EmailReceiver;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailReceiverRepository extends JpaRepository<Long, EmailReceiver> {
-
+public interface EmailReceiverRepository extends JpaRepository<EmailReceiver, Long> {
 }
