61a43e6c97849b05f9c3c814d7aef7d1ebe81492
chabin37 <7chabin@gmail.com>
2025-08-06 02:02:53 +0900

    chore: 바뀐 도메인 하드코딩으로 수정(추후 환경변수화 예정)

--- src/main/java/gdsc/konkuk/platformcore/global/consts/SPAConstants.java
+++ src/main/java/gdsc/konkuk/platformcore/global/consts/SPAConstants.java
@@ -2,7 +2,7 @@ package gdsc.konkuk.platformcore.global.consts;
 
 public class SPAConstants {
 
-    public static final String SPA_ADMIN_BASE_URL = "https://admin.gdsc-konkuk.dev";
+    public static final String SPA_ADMIN_BASE_URL = "https://admin.gdgoc-konkuk.com";
     public static final String SPA_ADMIN_LOGIN_REDIRECT_URL =
             SPA_ADMIN_BASE_URL + "/oauth/callback";
     public static final String SPA_ADMIN_ATTENDANCE_SUCCESS_REDIRECT_URL =
