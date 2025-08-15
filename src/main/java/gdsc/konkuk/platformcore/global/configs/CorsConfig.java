c6e9789c9a49286e89c4372340cf73861d740ecb
chabin37 <7chabin@gmail.com>
2025-08-15 17:09:04 +0900

    fix : 테스트 깨짐 보완
    - static과 주입받는 인스턴스 필드 같이 사용해서 테스트가 깨짐
    - CorsProps를 따로 만들어, allowOrigins 주입 보장

--- src/main/java/gdsc/konkuk/platformcore/global/configs/CorsConfig.java
+++ src/main/java/gdsc/konkuk/platformcore/global/configs/CorsConfig.java
@@ -3,9 +3,9 @@ package gdsc.konkuk.platformcore.global.configs;
 
 import java.util.Arrays;
 
-import gdsc.konkuk.platformcore.global.consts.PlatformConstants;
+import gdsc.konkuk.platformcore.global.props.CorsProps;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,15 +13,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(CorsProps.class)
 public class CorsConfig {
 
-    private final PlatformConstants platformConstants;
+    private final CorsProps corsProps;
 
     @Bean
     public UrlBasedCorsConfigurationSource getConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOriginPatterns(platformConstants.getAllowedOriginsList());
+        configuration.setAllowedOriginPatterns(corsProps.allowedOrigins());
         configuration.setAllowedMethods(
                 Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowCredentials(true);
--- src/main/java/gdsc/konkuk/platformcore/global/consts/PlatformConstants.java
+++ src/main/java/gdsc/konkuk/platformcore/global/consts/PlatformConstants.java
@@ -1,22 +1,6 @@
 package gdsc.konkuk.platformcore.global.consts;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Configuration
-@ConfigurationProperties(prefix = "cors")
 public class PlatformConstants {
-
-    private String allowedOrigins;
-
-    public List<String> getAllowedOriginsList() {
-        return Arrays.stream(allowedOrigins.split(","))
-                     .map(String::trim)
-                     .toList();
-    }
 
     public static final Integer SOFT_DELETE_RETENTION_MONTHS = 3;
     public static final String EMAIL_RECEIVER_NAME_REGEXP = "\\{이름}";
